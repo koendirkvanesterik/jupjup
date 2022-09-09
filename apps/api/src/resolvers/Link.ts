b2e7283d5090d81d052f67060762acf93479ec88
Koen Dirk van Esterik <info@koendirkvanesterik.com>
2022-09-09 20:01:58 +0200

    feat: add schema and resolvers

--- apps/api/src/resolvers/Link.ts
+++
@@ -1,8 +0,0 @@
-import { LinkResolvers } from '../generated/types'
-
-const postedBy: LinkResolvers['postedBy'] = (parent, _args, context) =>
-  context.prisma.link.findUnique({ where: { id: parent.id } }).postedBy()
-
-export const Link: LinkResolvers = {
-  postedBy,
-}
--- apps/api/src/resolvers/index.ts
+++
@@ -1,4 +0,0 @@
-export { Link } from './Link'
-export { Mutation } from './Mutation'
-export { Query } from './Query'
-export { User } from './User'
